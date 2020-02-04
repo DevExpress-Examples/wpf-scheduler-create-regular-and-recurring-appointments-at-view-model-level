c2a20b09b2ccf06520e64e95ef84452781c37246
Andrey Marten <andrey.kunitsyn@devexpress.com>
2020-02-04 17:02:12 +0300

    add example content

---
+++ CS/DXApplication14/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Xpf.Core;
+
+namespace DXApplication14
+{
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CS/DXApplication14/SchedulerService.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Mvvm.UI;
+using DevExpress.Xpf.Scheduling;
+
+namespace DXApplication14
+{
+    public interface ISchedulerService
+    {
+        void ShowEditAppointmentWindow();
+    }
+    public class SchedulerService : ServiceBase, ISchedulerService
+    {
+        public void ShowEditAppointmentWindow()
+        {
+            var scheduler = this.AssociatedObject as SchedulerControl;
+            if (scheduler != null)
+                scheduler.Commands.ShowAppointmentWindowCommand.Execute(null);
+        }
+    }
+}
