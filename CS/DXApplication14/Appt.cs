cbb8bcbdd523c1b8c6a988ed1ef86b229c3842f4
Andrey Marten <andrey.kunitsyn@devexpress.com>
2020-02-09 19:26:06 +0300

    Update example content

---
+++ CS/DXApplication14/Appt.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.Mvvm;
+using System;
+
+namespace DXApplication14
+{
+    public class Appt : BindableBase {
+
+
+        protected string _Subject;
+        public string Subject
+        {
+            get { return this._Subject; }
+            set { this.SetProperty(ref this._Subject, value, "Subject"); }
+        }
+
+
+        protected DateTime _Start;
+        public DateTime Start
+        {
+            get { return this._Start; }
+            set { this.SetProperty(ref this._Start, value, "Start"); }
+        }
+
+
+        protected DateTime _End;
+        public DateTime End
+        {
+            get { return this._End; }
+            set { this.SetProperty(ref this._End, value, "End"); }
+        }
+
+        protected string _Description;
+        public string Description
+        {
+            get { return this._Description; }
+            set { this.SetProperty(ref this._Description, value, "Description"); }
+        }
+
+
+        protected int _Type;
+        public int Type
+        {
+            get { return this._Type; }
+            set { this.SetProperty(ref this._Type, value, "Type"); }
+        }
+
+
+        protected string _RecurrenceInfo;
+        public string RecurrenceInfo
+        {
+            get { return this._RecurrenceInfo; }
+            set { this.SetProperty(ref this._RecurrenceInfo, value, "RecurrenceInfo"); }
+        }
+    }
+}
---
+++ CS/DXApplication14/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Xpf.Scheduling;
+using DevExpress.XtraScheduler;
+using System.Collections.ObjectModel;
+
+namespace DXApplication14
+{
+    public class MainViewModel 
+    {
+        public virtual ISchedulerService SchedulerService { get { return null; } }
+        public virtual DateTimeRange SelectedInterval { get; set; }
+
+        protected ObservableCollection<Appt> _AppointmentItems;
+        public ObservableCollection<Appt> AppointmentItems
+        {
+            get
+            {
+                if (this._AppointmentItems == null)
+                {
+                    this._AppointmentItems = new ObservableCollection<Appt>();
+                }
+
+                return this._AppointmentItems;
+            }
+        }
+
+        public virtual bool OpenWindow { get; set; } = true;
+
+        protected ObservableCollection<Appt> _SelectedAppointments;
+        public ObservableCollection<Appt> SelectedAppointments
+        {
+            get
+            {
+                if (this._SelectedAppointments == null)
+                {
+                    this._SelectedAppointments = new ObservableCollection<Appt>();
+                }
+
+                return this._SelectedAppointments;
+            }
+        }
+
+        protected Appt CreateAppt()
+        {
+            return new Appt()
+            {
+                Subject = "New Appt #" + AppointmentItems.Count,
+                Start = SelectedInterval.Start,
+                End = SelectedInterval.End,
+                Description = "[add description]"
+            };
+        }
+
+        public void AddAppt(bool recurrent = false)
+        {
+            var appt = CreateAppt();
+            appt.Type = (int)(recurrent ? AppointmentType.Pattern : AppointmentType.Normal);
+            appt.RecurrenceInfo = recurrent ? RecurrenceBuilder.Daily(SelectedInterval.Start, 10).Build().ToXml() : string.Empty;
+            this.AppointmentItems.Add(appt);
+            this.SelectedAppointments.Clear();
+            this.SelectedAppointments.Add(appt);
+            if (SchedulerService != null && OpenWindow)
+                SchedulerService.ShowEditAppointmentWindow();
+        }
+    }
+}
