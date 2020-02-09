e890f1743e1fde26004e40c9f2f61a6e3a658e8c
DevExpressExampleBot <fakeEmail>
2020-02-09 19:29:51 +0300

    Source auto update [skip ci]

---
+++ VB/DXApplication14/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports DevExpress.Xpf.Core
+
+Namespace DXApplication14
+	Partial Public Class MainWindow
+		Inherits ThemedWindow
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/DXApplication14/SchedulerService.vb
@@ -0,0 +1,19 @@
+﻿Imports DevExpress.Mvvm.UI
+Imports DevExpress.Xpf.Scheduling
+
+Namespace DXApplication14
+	Public Interface ISchedulerService
+		Sub ShowEditAppointmentWindow()
+	End Interface
+	Public Class SchedulerService
+		Inherits ServiceBase
+		Implements ISchedulerService
+
+		Public Sub ShowEditAppointmentWindow() Implements ISchedulerService.ShowEditAppointmentWindow
+			Dim scheduler = TryCast(Me.AssociatedObject, SchedulerControl)
+			If scheduler IsNot Nothing Then
+				scheduler.Commands.ShowAppointmentWindowCommand.Execute(Nothing)
+			End If
+		End Sub
+	End Class
+End Namespace
