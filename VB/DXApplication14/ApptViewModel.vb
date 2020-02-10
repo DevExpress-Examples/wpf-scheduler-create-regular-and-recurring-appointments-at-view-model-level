6bd42959b383c120b5f7f77e90b4dd7b24d29aa5
DevExpressExampleBot <fakeEmail>
2020-02-10 23:45:41 +0300

    Source auto update [skip ci]

---
+++ VB/DXApplication14/ApptViewModel.vb
@@ -0,0 +1,57 @@
+﻿Imports DevExpress.Mvvm
+Imports System
+
+Namespace DXApplication14
+	Public Class ApptViewModel
+		Inherits BindableBase
+
+		Public Property Subject() As String
+			Get
+				Return GetValue(Of String)()
+			End Get
+			Set(ByVal value As String)
+				SetValue(value)
+			End Set
+		End Property
+		Public Property Start() As DateTime
+			Get
+				Return GetValue(Of DateTime)()
+			End Get
+			Set(ByVal value As DateTime)
+				SetValue(value)
+			End Set
+		End Property
+		Public Property [End]() As DateTime
+			Get
+				Return GetValue(Of DateTime)()
+			End Get
+			Set(ByVal value As DateTime)
+				SetValue(value)
+			End Set
+		End Property
+		Public Property Description() As String
+			Get
+				Return GetValue(Of String)()
+			End Get
+			Set(ByVal value As String)
+				SetValue(value)
+			End Set
+		End Property
+		Public Property Type() As Integer
+			Get
+				Return GetValue(Of Integer)()
+			End Get
+			Set(ByVal value As Integer)
+				SetValue(value)
+			End Set
+		End Property
+		Public Property RecurrenceInfo() As String
+			Get
+				Return GetValue(Of String)()
+			End Get
+			Set(ByVal value As String)
+				SetValue(value)
+			End Set
+		End Property
+	End Class
+End Namespace
---
+++ VB/DXApplication14/MainViewModel.vb
@@ -0,0 +1,66 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.DataAnnotations
+Imports DevExpress.Xpf.Scheduling
+Imports DevExpress.XtraScheduler
+Imports System
+Imports System.Collections.ObjectModel
+
+Namespace DXApplication14
+	Public Class MainViewModel
+		Inherits ViewModelBase
+
+		Public Property Interval() As DateTimeRange
+			Get
+				Return GetValue(Of DateTimeRange)()
+			End Get
+			Set(ByVal value As DateTimeRange)
+				SetValue(value)
+			End Set
+		End Property
+		Public Property Appointments() As ObservableCollection(Of ApptViewModel)
+			Get
+				Return GetValue(Of ObservableCollection(Of ApptViewModel))()
+			End Get
+			Set(ByVal value As ObservableCollection(Of ApptViewModel))
+				SetValue(value)
+			End Set
+		End Property
+		Public Property SelectedAppointments() As ObservableCollection(Of ApptViewModel)
+			Get
+				Return GetValue(Of ObservableCollection(Of ApptViewModel))()
+			End Get
+			Set(ByVal value As ObservableCollection(Of ApptViewModel))
+				SetValue(value)
+			End Set
+		End Property
+		Public Sub New()
+			Appointments = New ObservableCollection(Of ApptViewModel)()
+			SelectedAppointments = New ObservableCollection(Of ApptViewModel)()
+		End Sub
+		Protected Function CreateAppt(ByVal subj As String, ByVal start As DateTime, ByVal [end] As DateTime, ByVal description As String) As ApptViewModel
+			Dim apptViewModel As New ApptViewModel() With {
+				.Subject = subj,
+				.Start = start,
+				.End = [end],
+				.Description = "[add description]"
+			}
+			Return apptViewModel
+		End Function
+
+		<Command>
+		Public Sub AddAppt(Optional ByVal recurrent As Boolean = False)
+			Dim appt = CreateAppt($"New Appt #{Appointments.Count}", Interval.Start, Interval.End, "[add description]")
+
+			If recurrent Then
+				appt.Type = CInt(Math.Truncate(AppointmentType.Pattern))
+				appt.RecurrenceInfo = RecurrenceBuilder.Daily(Interval.Start, 10).Build().ToXml()
+			Else
+				appt.Type = CInt(Math.Truncate(AppointmentType.Normal))
+			End If
+
+			Me.Appointments.Add(appt)
+			Me.SelectedAppointments.Clear()
+			Me.SelectedAppointments.Add(appt)
+		End Sub
+	End Class
+End Namespace
